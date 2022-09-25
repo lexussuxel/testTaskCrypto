02fe87fabba31992a1ad925e8ec985466eaf18cb
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2022-09-25 14:55:20 +0300

    Add hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {persist, store} from "./store";
 import {PersistGate} from "redux-persist/integration/react";
@@ -13,9 +13,9 @@ root.render(
   <React.StrictMode>
       <Provider store={store}>
           <PersistGate loading={null} persistor={persist}>
-              <BrowserRouter>
+              <HashRouter>
                   <App />
-              </BrowserRouter>
+              </HashRouter>
           </PersistGate>
       </Provider>
 
